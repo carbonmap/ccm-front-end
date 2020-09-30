fedab3c192517a10d8be745bb50a5d17cc14156f
asarjou <armansarjou@icloud.com>
2020-09-30 12:00:36 +0100

    Functioning Accordion

--- src/app/modules/acc/acc.component.ts
+++ src/app/modules/acc/acc.component.ts
@@ -6,12 +6,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./acc.component.scss']
 })
 export class AccComponent implements OnInit {
+  data  = [ 
 
-  title = 'Colleges';
-  entites = ['Kings', 'Homerton', 'Girton', 'Trinity'];
-  myEntity = this.entites[0]; 
-  i
-  ngOnInit(): void {
+    {
+        name: "King's College, Cambridge",
+        id: "uk.ac.cam.kings",
+        joined: 1234568,
+        latest_data: 1234568,
+        parent_entity: null,
+        properties: [{ 
+           "Year Established": 1350,
+           "Number of Students": 1500
+        }]
+    },
+    {
+        name: "King's College Gardens",
+        id: "uk.ac.cam.kings.gardens",
+        joined: "1234568",
+        latest_data: "1234568",
+        parent_entity: "uk.ac.cam.kings",
+        properties: [{
+           "Year Established": 1350,
+           "Number of Employees": 8,
+           "Number of Cars": 4
+        }]
+    },
+    {
+       name: "King's College Chapel",
+       id: "uk.ac.cam.kings.cathedral",
+       joined: 5623451,
+       latest_data: 32435467,
+       parent_entity: "uk.ac.cam.kings",
+       properties: [{
+          "Year Established": 1312, 
+          "Number of Employees": 32
+       }]
+    }
+  ];
+  
+  titles = ['Entity ID', 'Date Joined', 'Last Modified', 'Entity Name', 'Parent Entity']; 
+  
+  ngOnInit(): void { console.log()
+
+  
+
   }
-
 }
